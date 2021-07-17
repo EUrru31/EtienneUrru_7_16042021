dd0c5f76fbc1f45b1f95c3a7440c45580ad51386
Urru <etienne.urru@gmail.com>
2021-07-17 22:49:42 +0200

    Frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app">
+        <loginPage></loginPage>
+    </div>
+</template>
+
+<script>
+import loginPage from "./components/loginPage.vue";
+
+export default {
+    name: "loginPage",
+    components: "loginPage",
+};
+</script>
---
+++ frontend/src/components/loginPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class container>
+        <h1>Login</h1>
+        <input type="email" value="E-mail" required />
+        <input type="password" value="Mot de passe" required />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "loginPage",
+};
+</script>
+
+<style scoped lang="scss"></style>
