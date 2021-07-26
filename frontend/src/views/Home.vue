6aa7e23491f07a9ea3b3efbc6c103dcca567a6e5
Urru <etienne.urru@gmail.com>
2021-07-26 19:39:46 +0200

    Fontawesome + front wall

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>Page d'acceuil</h1>
+    <div>
+        <h1>Page d'acceuil</h1>
+        <img class="logo" alt="logo groupomania" src="icon-above-font.svg" />
+        <template class="card_field">
+            <p>Users</p>
+            <h3>Title</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+                beatae, ullam perspiciatis enim, sequi quae reiciendis id
+                architecto aspernatur aliquam numquam molestias quo ab placeat
+                debitis minima suscipit doloremque assumenda!
+            </p>
+            <div class="like">
+                <i class="far fa-heart fa-lg"></i>
+                <i class="fas fa-heart"></i>
+            </div>
+        </template>
+    </div>
 </template>
+
+<script>
+export default {
+    name: "Home",
+};
+</script>
