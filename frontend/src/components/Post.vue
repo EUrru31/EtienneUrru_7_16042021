a1f1dcfa925cdc1bc6e3cda6c2c5e2990dc33006
Urru <etienne.urru@gmail.com>
2021-07-29 17:06:08 +0200

    posts

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,9 +1,32 @@
 <template>
-    <div>
-        <h3>{{ postData.title }}</h3>
-        <p>{{ postData.text }}</p>
-        <div v-if="isEditable" @click.prevent="editPost()">
-            <i class="fas fa-edit"></i>
+    <div class="posts">
+        <div class="postIcon">
+            <div class="post">
+                <h3>{{ postData.title }}</h3>
+                <p>{{ postData.text }}</p>
+            </div>
+            <div class="icons">
+                <div v-if="isEditable" @click.prevent="editPost()">
+                    <i class="fas fa-edit"></i>
+                </div>
+                <div v-if="isLikable" @click.prevent="likePost()">
+                    <i class="fas fa-heart"></i>
+                </div>
+                <div v-if="isCommentable" @click.prevent="commentPost()">
+                    <i class="fas fa-comment-alt"></i>
+                </div>
+                <div v-if="isDeletable" @click.prevent="deletePost()">
+                    <i class="fas fa-trash"></i>
+                </div>
+            </div>
+        </div>
+        <div>
+            <input
+                class="input content"
+                autocomplete="text"
+                placeholder="Commentaire"
+            />
+            <button>Commentez</button>
         </div>
     </div>
 </template>
@@ -34,14 +57,60 @@ export default {
             }
             return false;
         },
+        isLikable() {
+            if (this.$store.state.user.isAdmin === 1) {
+                return true;
+            }
+            if (this.postData.user_id === this.$store.state.user.user.id) {
+                return true;
+            }
+            return false;
+        },
+        isCommentable() {
+            if (this.$store.state.user.isAdmin === 1) {
+                return true;
+            }
+            if (this.postData.user_id === this.$store.state.user.user.id) {
+                return true;
+            }
+            return false;
+        },
     },
     methods: {
         editPost() {
-            console.log("coucou");
+            console.log("post edité");
+        },
+        likePost() {
+            console.log("post liké");
+        },
+        commentPost() {
+            console.log("post commenté");
+        },
+        deletePost() {
+            console.log("post supprimé");
         },
     },
     mounted() {},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+i {
+    cursor: pointer;
+    margin: 10px;
+}
+.icons {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+}
+.postIcon {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+.post {
+    display: flex;
+    flex-direction: column;
+}
+</style>
