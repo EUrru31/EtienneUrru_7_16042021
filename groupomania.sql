cee52dfb80bbf45f925ea4a537388f2286937552
Urru <etienne.urru@gmail.com>
2021-08-02 19:32:17 +0200

    base de donnée

--- groupomania.sql
+++ groupomania.sql
@@ -16,14 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `groupomania`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `groupomania` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `groupomania`;
-
---
 -- Table structure for table `comments`
 --
 
@@ -34,14 +26,13 @@ CREATE TABLE `comments` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` smallint(5) unsigned NOT NULL,
   `posts_id` smallint(5) unsigned NOT NULL,
-  `title` varchar(255) NOT NULL,
   `text` varchar(2000) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `posts_id` (`posts_id`),
   CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`posts_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,6 +41,7 @@ CREATE TABLE `comments` (
 
 LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (16,42,35,'Merci pour cette initiative!'),(17,41,37,'Bienvenue !!!');
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,12 +82,11 @@ CREATE TABLE `posts` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` smallint(5) unsigned NOT NULL,
   `text` varchar(2000) DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL,
   `likes` smallint(5) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,6 +95,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (35,41,'Bienvenue sur le nouveau réseau social de la société Groupomania !!!',NULL),(37,42,'Premier post sur le site',NULL),(45,46,'Bonjour, Je m\'appelle Luc. Je suis nouveau ici :)',NULL),(46,47,'Vendredi soir, Apéro à la compta, on compte sur vous !!!!!',NULL);
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,7 +115,7 @@ CREATE TABLE `users` (
   `password` varchar(80) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,7 +124,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (17,'Urru','Etienne','etienne.urru@gmail.com',0,'$2b$10$FS/88F.K/cT8G6H770U4veaV5sk0VbBMGT6EEjqfVzW/osaym3hDu'),(23,'Groupomania','Admin','communication@groupomania.com',0,'$2b$10$5tJs7lj6en0mMJFM/r5GW.WKJWsUb2HMwrs8Bwi0P6dRsa3t222wq');
+INSERT INTO `users` VALUES (41,'Groupomania','Admin','admin@groupomania.com',1,'$2b$10$Vef37qMXzrhwN9Ft.GYBMe1cfeINTAtSc6iENRns62R0OcqRbacli'),(42,'Urru','Etienne','etienne.urru@gmail.com',NULL,'$2b$10$v4iLWf1fNEP8R0MTkyYogO6SOLu.2VO5KynkZlV8zE8pYTqWZT5sS'),(46,'Premier','Luc','lucpremier@groupomania.com',NULL,'$2b$10$byWESq4Z/5eYMz3Az.Qju.66KMJ4ae38JZhCcqxAINEsvBoMeXC5m'),(47,'Seconde','Justine','justineseconde@groupomania.com',NULL,'$2b$10$eiCEQ8LZdsgBhiYm.L7rT.Mf0j1Ds3TOepNyoAi0pMsdGqXnMuFba');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -145,4 +137,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-28 11:00:17
+-- Dump completed on 2021-08-02 19:31:05
