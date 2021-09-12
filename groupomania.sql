df21b899d4d2b3b54750c89e8f9a6d88ad3596e6
Urru <etienne.urru@gmail.com>
2021-09-13 01:40:52 +0200

    13092021

--- groupomania.sql
+++ groupomania.sql
@@ -15,6 +15,16 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+--
+-- Current Database: `groupomania`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `groupomania` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `groupomania`;
+
+
 --
 -- Table structure for table `comments`
 --
